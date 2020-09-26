1a9a5eaf1f5b4a09a6d9d5012ef6d8a91ac699fe
Gento <a551.return@gmail.com>
2020-09-26 17:50:15 +0900

    Home画面作成

--- src/App.vue
+++ src/App.vue
@@ -25,32 +25,22 @@
         />
       </div>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <Home/>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Home from './components/Home';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Home,
   },
 
   data: () => ({
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                ホーム画面
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+}
+</script>
