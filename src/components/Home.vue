432fe2e08919111dd82ab9f931ca4339729a459b
Gento <a551.return@gmail.com>
2020-09-27 20:16:02 +0900

    映画情報を取得できた

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,7 +9,29 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'Home',
+    data() {
+        return {
+            movies: [],
+            apiKey: 'a1a357b8cd4732e4d9c84ecc9a1d7406',
+        }
+    },
+    methods: {
+        getMovies() {
+            axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${this.apiKey}&page=1`)
+            .then(response => {
+                this.movies = response.data.results
+                console.log(this.movies);
+            })
+            console.log(this.movies);
+
+        },
+    },
+    created() {
+        this.getMovies();
+    }
 }
 </script>
