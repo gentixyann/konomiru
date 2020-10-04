0849539dda647c6720451bc35f87caa3f262ae91
Gento <a551.return@gmail.com>
2020-10-04 22:34:32 +0900

    firebaseプロジェクトを作成

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
-import VueRouter from 'vue-router';
+import VueRouter from 'vue-router'
+import firebase from 'firebase'
 import { routes } from './route';
 
 Vue.config.productionTip = false
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAGNUWmcdndD9eqFPAVGMohflGv9tjNNg8",
+  authDomain: "konomiru-429d7.firebaseapp.com",
+  databaseURL: "https://konomiru-429d7.firebaseio.com",
+  projectId: "konomiru-429d7",
+  storageBucket: "konomiru-429d7.appspot.com",
+  messagingSenderId: "44258461880",
+  appId: "1:44258461880:web:43d4e46eb645daa937cc4d",
+  measurementId: "G-KJQ26V0WBS"
+};
+firebase.initializeApp(firebaseConfig);
+
 
 Vue.use(VueRouter);
 
